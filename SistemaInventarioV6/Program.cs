ce58d1a00034d4c696161993e3515a8ef939897b
DevUrqui <devurqui@gmail.com>
2023-09-07 08:05:56 +0200

    Agregar area Inventario, configurar esta misma y añadir archivos al mismo

--- SistemaInventarioV6/Program.cs
+++ SistemaInventarioV6/Program.cs
@@ -45,7 +45,7 @@ namespace SistemaInventarioV6
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{Area=Inventario}/{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
